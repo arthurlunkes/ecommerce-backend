a0d5d969eac67e5d87529ca06b9f7ba7e86682dd
Arthur Lunkes <arthur.lunkes2017@gmail.com>
2025-10-26 15:21:38 -0300

    Remove credentials.

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,11 +15,11 @@ import { OrderModule } from './cases/orders/order.module'
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'aws-0-sa-east-1.pooler.supabase.com',
-      port: 5432,
-      username: 'postgres.nmxomntywnwtroavsxne',
-      password: 'postgres',
-      database: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
+      database: process.env.DB_NAME || 'postgres',
       // Auto carregar entidades
       autoLoadEntities: true,
       // Sincronizar com o banco, todas alterações são refletidas no banco
